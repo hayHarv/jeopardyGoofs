b530501580ff74080dcb2ed9fa900086c83fecc0
hayHarv <hsharvey@gmail.com>
2017-09-25 20:37:59 -0700

    Added command line game

--- src/random_clue/random_clue.py
+++ src/random_clue/random_clue.py
@@ -1,8 +1,13 @@
+#TODO filter final jeopardy
+#TODO make dem functions better
+#TODO add prompt for # of questions (and include final jeopardy?)
+#TODO add timer
+
 import sqlite3
 import os
 import numpy as np
 
-data_path = './Data/jeopardy.sqlite'
+data_path = '/home/haydenh/repos/projects/jeopardy/Data/jeopardy.sqlite'
 conn = sqlite3.connect(data_path)
 c = conn.cursor()
 rows = c.execute('select count(*) from questions where value>0;').fetchone()[0]
@@ -21,6 +26,28 @@ def get_question(rows, sqlite_cursor):
     value = result[1]
     question = result[2]
     answer = result[3]
-    # print for debug
-    print('{} {} {} {}'.format(category, value, question, answer))
+    return category, value, question, answer
 
+
+def play(rows, sqlite_cursor):
+    cat, val, q, ans = get_question(rows, sqlite_cursor)
+    user ='y' 
+    if user == 'y':
+        print("Category: {}".format(cat))
+        print("Value: ${}".format(val))
+        print("{}".format(q))
+        user = input("Type [a] if you're ready for the answer: ")
+        if user == 'a':
+            print(ans)
+            return ""
+
+def keep_playing(rows, sqlite_cursor):
+    play(rows, sqlite_cursor)
+    user = input("play another? [y/n] ")
+    if user == 'y':
+       keep_playing(rows, sqlite_cursor)
+    else:
+        return ""
+
+if __name__=="__main__":
+    keep_playing(rows, c)
