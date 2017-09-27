91f04223b7379d02ecbe05d630b058cb476103c3
hayHarv <hsharvey@gmail.com>
2017-09-27 14:06:29 -0700

    edited

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 setup(name='jeopardy',
       version='0.1',
       packages=find_packages(),
-      install_requires=['numpy', 'sqlite']
+      install_requires=['numpy', 'sqlite3']
       )
