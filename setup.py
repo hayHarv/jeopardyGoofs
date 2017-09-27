02845566525bc4fdc9eb0eea44fad0f4ecb07a93
hayHarv <hsharvey@gmail.com>
2017-09-27 14:11:37 -0700

    add sqlite version to setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 setup(name='jeopardy',
       version='0.1',
       packages=find_packages(),
-      install_requires=['numpy', 'sqlite3']
+      install_requires=['numpy', 'sqlite==3.13.0']
       )
